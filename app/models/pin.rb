fdc1fbf77a5c619e6b51c6f2a6338cabf7d1fce6
Brad <brad.engelhardt@gmail.com>
2014-10-17 21:08:10 -0700

    added user avatars, added placeholder like and comment buttons, adjusted some css

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,7 +2,7 @@ class Pin < ActiveRecord::Base
 	Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.8.9-Q16'
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100#" }
 
 	validates :image, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
 
   has_many :pins
 
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   validates :name, presence: true
   validates :name, uniqueness: true
 end
