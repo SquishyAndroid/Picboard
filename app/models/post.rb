7fb8ecf18755847b9e7bdfb336863e2fee2df3ae
Brad <brad.engelhardt@gmail.com>
2015-01-11 14:05:42 -0800

    remove zoom on input for ios8 and list comments first to last

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
-	has_many :comments, :order => 'created_at DESC'
+	has_many :comments
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100#" }
 
